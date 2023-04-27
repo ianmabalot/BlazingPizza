53099c37856908fe6f5dc96f8b3a735f7635df3a
Ian Benedict A. Mabalot <ianbenedictmabalot@gmail.com>
2023-04-27 18:08:16 +0800

    initial commit

---
+++ Services/PizzaService.cs
@@ -0,0 +1,12 @@
+
+using BlazingPizza.Data;
+
+namespace BlazingPizza.Services;
+    public class PizzaService
+    {
+        public async Task<Pizza[]> GetPizzasAsync()
+        {
+        // Call your data access technology here
+            return null;
+        }
+    }
